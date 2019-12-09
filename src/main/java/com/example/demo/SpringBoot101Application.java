49ddc948d5669fa7d2594c6c4b78ace6aec581b4
ambriel2016 <crb3305@gmail.com>
2019-12-09 13:58:12 -0500

    1

---
+++ src/main/java/com/example/demo/SpringBoot101Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+gitSpringBootApplication
+public class SpringBoot101Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot101Application.class, args);
+    }
+
+}
